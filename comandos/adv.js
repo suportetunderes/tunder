2b88d11c1d9c21ac615d4a07a0268c43dc5de875
Glitch (tunder-store) <none>
2020-06-03 21:17:56 +0000

    🗻🎍 Checkpoint ./comandos/adv.js:15634585/3359

--- comandos/adv.js
+++ comandos/adv.js
@@ -4,55 +4,26 @@ const config = require("../config.json");
 exports.run = async (bot,message,args) => { 
 
     message.delete();
+    let splitarg = args.join(" ").split(" / ");
 
     let member = message.mentions.members.first()
     if(!member)
       return message.reply("Por favor mencione um usuário válido !") 
 
-      .catch(error => message.reply(`Desculpe ${message.author} não consegui banir o membro devido o: ${error}`))
+    let reason = splitarg[0];
+    if(!reason) reason = "Nenhuma razão fornecida"
 
-    if(args[0] === "1") {
-        let ADV1 = new Discord.RichEmbed()
-        .setTitle(`:x: | **__Advertência__**`)
-        .addField(":x: | **Punido** »", `\`${member.user.tag}\``)
-        .addField(":white_check_mark: | **Author** »", `\`${message.author.tag}\``)
-        .addField(":white_small_square:  | **Motivo** »", `\`${args}\``)
-        .addField("🔗  | **Advertência** »", `\`1\``)
-        
-     message.channel.send(ADV1)
-    }
+    let advt = splitarg[1];
+    if(!advt) advt = "Isso Foi So Um Aviso"
 
-    if(args[0] === "2") {
-        
-        let ADV2 = new Discord.RichEmbed()
-        .setTitle(`:x: | **__Advertência__**`)
-        .addField(":x: | **Punido** »", `\`${member.user.tag}\``)
-        .addField(":white_check_mark: | **Author** »", `\`${message.author.tag}\``)
-        .addField(":white_small_square:  | **Motivo** »", `\`${args}\``)
-        .addField("🔗  | **Advertência** »", `\`2\``)
-        
-     message.channel.send(ADV2)
-       }
+    let ADV = new Discord.RichEmbed()
+    .setTitle(`:x: | **__Advertência__**`)
+    .addField(":x: | **Punido** »", `\`${member.user.tag}\``)
+    .addField(":white_check_mark: | **Author** »", `\`${message.author.tag}\``)
+    .addField(":white_small_square:  | **Motivo** »", `\`${reason}\``)
+    .addField("🔗  | **Advertência** »", `\`${advt}\``)
     
-    if(args[0] === "3") {
-        
-        let ADV3 = new Discord.RichEmbed()
-        .setTitle(`:x: | **__Advertência__**`)
-        .addField(":x: | **Punido** »", `\`${member.user.tag}\``)
-        .addField(":white_check_mark: | **Author** »", `\`${message.author.tag}\``)
-        .addField(":white_small_square:  | **Motivo** »", `\`${args}\``)
-        .addField("🔗  | **Advertência** »", `\`3\``)
-        
-     message.channel.send(ADV3)
-       }
-
-    if(!args[0]) {
-        return message.channel.send(`
-${member} Utilize Qual ADV Ele Vai Receber.
-
-EX:
-${config.prefix}adv \`1\`, \`2\` e \`3\``).then(msg => msg.delete(7000));
-    }
+ message.channel.send(ADV)
 
 }
 
