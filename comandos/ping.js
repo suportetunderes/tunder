2f2b4a880f2e1311ecb73089f46aebed53c3fee9
Glitch (tunder-store) <none>
2020-06-05 02:34:19 +0000

    🎐💗 Checkpoint ./comandos/ping.js:15634585/630

---
+++ comandos/ping.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+exports.run = (bot,message,args) => {
+
+    message.delete()
+
+    let gAvatar = message.guild.iconURL;
+    let embed = new Discord.RichEmbed()
+
+    .setTitle(`Ping Do Bot: 
+A latência da API é **${Math.round(bot.ping)}ms**`)
+    .setColor("#008aff")
+    .setTimestamp()
+    .setFooter(`${message.guild.name}`)
+
+    message.channel.send(embed);
+}
+
+exports.help = {
+    name: "ping",
+    aliases: []
+}
