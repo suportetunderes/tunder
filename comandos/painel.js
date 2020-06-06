dce8e1f3bd552d9868b153c3803113db79b3982e
Glitch (tunder-store) <none>
2020-06-06 19:32:51 +0000

    🌔🚉 Checkpoint ./comandos/painel.js:15634585/501 ./comandos/setcanal.js:15634585/633

--- comandos/painel.js
+++ comandos/painel.js
@@ -7,8 +7,10 @@ message.delete()
 const canalbemvindo = db.get(`canalbemvindo_${message.guild.id}`)
   
    let embed = new Discord.RichEmbed()
+   .setColor("#008aff")
    .setTitle(`Painel De Configuração`)
-   .addField(`Modelo Entrada`,`Canal <#${canalbemvindo}>`)
+   .addField(`**<a:entrou:714207447354441768>Modelo Entrada**`,`<:ativado:718906937168888018>**Status:** Ativo\n<:texto:718906937298911262>**Canal:** <#${canalbemvindo}>`)
+   .addField(`**<a:saiu:714207447371350057>Modelo Saida**`,`<:desativado:718906937139396668>**Status:** Desativado\n<:texto:718906937298911262>**Canal:** \`Sem Canal\``)
 
 message.channel.send(embed)
 
--- comandos/setcanal.js
+++ comandos/setcanal.js
@@ -5,11 +5,17 @@ exports.run = (bot,message,args) => {
 
 message.delete()
 
+
   if(args[0] == 'bemvindo') {
 const canalbemvindo = message.mentions.channels.first()
     
   db.set(`canalbemvindo_${message.guild.id}`, canalbemvindo.id)
-message.reply("setado!")
+
+  const canalbemvindo1 = db.get(`canalbemvindo_${message.guild.id}`)
+  
+message.reply(`**Canal De Bem-Vindo Setado Em**
+<:texto:718906937298911262>${canalbemvindo}`)
+}
      }
 
 }
