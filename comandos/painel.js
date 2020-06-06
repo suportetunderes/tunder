3f5d4df2932f52e72c9c62440805405850a22c2f
Glitch (tunder-store) <none>
2020-06-06 19:20:54 +0000

    😹📌 Checkpoint ./comandos/painel.js:15634585/174

--- comandos/painel.js
+++ comandos/painel.js
@@ -2,11 +2,16 @@ const db = require('quick.db')
 const Discord = require('discord.js')
 
 exports.run = async (bot, message, args, config) => {
-    message.delete()
+message.delete()
+  
+const canalbemvindo = db.get(`canalbemvindo_${message.guild.id}`)
   
    let embed = new Discord.RichEmbed()
    .setTitle(`Painel De Configuração`)
-   .addField(``)
+   .addField(`Modelo Entrada`,`Canal <#${canalbemvindo}>`)
+
+message.channel.send(embed)
+
 }
 module.exports.help = { 
   name: "painel",
