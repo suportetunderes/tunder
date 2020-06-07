1d97f02a48bb0ef7b810881cbaee3c7cf06b97f2
Glitch (tunder-store) <none>
2020-06-07 03:01:06 +0000

    🐩⛽️ Checkpoint ./package.json:15634585/1493 ./comandos/play.js:15634585/899

--- comandos/play.js
+++ comandos/play.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const ytdl = require('ytdl-core')
+const pesquisa = require( 'yt-search' )
  
 module.exports.run = async (bot, message, args) => {
 
@@ -7,23 +8,26 @@ module.exports.run = async (bot, message, args) => {
   
   if(!message.member.voiceChannel) return message.channel.send('entre em um canal')
   if(message.guild.me.voiceChannel) return message.channel.send('ja estou em um canal')
-  if(!args[0]) return message.reply('coloque um URL válido')
 
-let validate = await ytdl.validateURL(args[0]);
-if(!validate) return message.channel.send('coloque um URL valido para reproduzir')
+let pes = args.join(" ")
+if(!pes) return message.reply('você não digitou um video valido')
 
-let info = await ytdl.getInfo(args[0])
-let length_seconds = info.length_seconds
-console.log(info)
-let connection = await message.member.voiceChannel.join();
-await message.delete().catch(O_o=>{})
-let dispatcher = await connection.playStream(ytdl(args[0], {
-filter: 'audioonly'
-}));
+pesquisa(pes, async (erro, re) => {
+if (erro) console.log(erro)
+
+const videos = re.videos;
+const pVideos = videos[0];
+
+const con = await message.member.voiceChannel.join();
+const musica = await con.playStream(y)
+})
+  
+  
+  
 let embed = new Discord.RichEmbed()
 .setTitle('Tocando:')
-.addField('Música:', `${info.title}`)
-.setThumbnail(info.thumbnailURL)
+.addField('Música:', ``)
+.setThumbnail()
 
 message.channel.send(embed);
    
