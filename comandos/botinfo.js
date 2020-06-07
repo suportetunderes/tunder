8d20abb44038c5e8cb857b0855016f4662ce408b
Glitch (tunder-store) <none>
2020-06-07 16:11:53 +0000

    🌖🧀 Checkpoint ./comandos/botinfo.js:15634585/562

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -11,19 +11,23 @@ module.exports.run = async (bot, message, args) => {
   .setColor("2F3136")
   .setTitle("**Informações - Dudinha⚡**")
   .setDescription(`Olá Minha Informações Esta Abaixo:`)
-  .addField(`__**Informações pessoais:**__`,`
+  .addField(`<:identidade:714207437875445822>__**Informações pessoais:**__`,`
 <:crashar:714207437854605352>Meu Nome: 
 <:papel:714207438588477501>Data De Nascimento: **6 de Junho de 2020**
 <:pessoa:714207437623787561>Meu Criador: <@652277720688099340>
 <:coroa:714207437632176259>Meu ID: 
 `)
-  .addField(`__**Estatísticas:**__`,`
+  .addField(`<:infoo:714207437976240321>__**Estatísticas:**__`,`
 <:550502619345453067:718906937063768135>Servidores: **${bot.guilds.size}**
 <:canal:714207437812531212>Canais: **${bot.channels.size}**
 <:grupo:714207438210990090>Usuários: **${bot.users.size}**`)
-  .addField(`__**Sobre Mim:**__`,`a`)
-  .addField(`a`,`a`)
-  .addField(`a`,`a`)
+  .addField(`<:dono:719218839874830416>__**Sobre Mim:**__`,`<a:infos:718906937349111858>Meu Prefixo: **tn.**
+<:js:719218839245553685>Linguagem: \`JavaScript\`
+<:dj:719219220541472891>Livraria: \`Discord.JS\``)
+  .addField(`<:plis:718906937873530981>__**Links úteis:**__`,`<:adicionar:718906937475072090>Convite: **(Clique Aqui)**[]
+<:bot:719219219996344322>Suporte: **(Clique Aqui)**[]
+<:sim_T:714207438408122441>Vote-Em-Mim: **(Clique Aqui)**[]`)
+
 
     message.channel.send(botembed);
 }
