664db04cd6448499c8a6ca21dbae63ff2da35956
Glitch (tunder-store) <none>
2020-06-07 02:48:54 +0000

    ☺️🛬 Checkpoint ./comandos/play.js:15634585/1144

--- comandos/play.js
+++ comandos/play.js
@@ -1,10 +1,31 @@
 const Discord = require("discord.js");
 const ytdl = require('ytdl-core')
  
-module.exports.run = (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
   message.delete();
+  
+  if(!message.member.voiceChannel) return message.channel.send('entre em um canal')
+  if(message.guild.me.voiceChannel) return message.channel.send('ja estou em um canal')
+  if(!args[0]) return message.reply('coloque um URL válido')
 
+let validate = await ytdl.validateURL(args[0]);
+if(!validate) return message.channel.send('coloque um URL valido para reproduzir')
+
+let info = await ytdl.getInfo(args[0])
+let length_seconds = info.length_seconds
+console.log(info)
+let connection = await message.member.voiceChannel.join();
+await message.delete().catch(O_o=>{})
+let dispatcher = await connection.playStream(ytdl(args[0], {
+filter: 'audioonly'
+}));
+let embed = new Discord.RichEmbed()
+.setTitle('Tocando:')
+.addField('Música:', `${info.title}`)
+.setThumbnail(info.thumbnailURL)
+
+message.channel.send(embed);
    
 };
  
