fadf0a06e55c920eadc768ea1a8d6569124b02e3
Glitch (tunder-store) <none>
2020-06-07 04:46:46 +0000

    🍫⛷ Checkpoint ./comandos/play.js:15634585/544

--- comandos/play.js
+++ comandos/play.js
@@ -16,6 +16,23 @@ if (erro) console.log(erro)
 
 const videos = re.videos;
 const pVideo = videos[0];
+
+let data = ops.active.get(message.guild.id);
+if (!data.connection) data.connection = await message.member.voiceChannel.join();
+if (!data.fila) data.fila = [];
+data.guildID = message.guild.id;
+
+data.fila.push({
+tituloMusica: pVideo.title,
+url: pVideo.url,
+views: pVideo.views,
+tempo: pVideo.duration.timestamp,
+author: message.author
+});
+
+if (!data.dispatcher) play(bot, ops, data);
+else {
+message.channel.send(`Adicionada a Fila ${pVideo.title}\nPedido Por: ${message.author}`)}
   
 })
 };
